b8b0793660ef39809663710306745327f1bbd985
Clive <viljoen.clive.g@gmail.com>
2021-03-06 12:30:05 +0200

    test

--- index.php
+++ index.php
@@ -3,13 +3,16 @@
 /*
  *
  */
-require_once __DIR__. '/vendor/autoload.php';
+require_once __DIR__.'/vendor/autoload.php';
 use app\core\Application;
 
 $app = new Application();
 
-$app->router->get('/',function(){
+$app->router->get('/', function(){
     return 'Hello World';
+});
+$app->router->get('/contact', function(){
+    return 'Contact';
 });
 
 $app->run();
