88d8f53163b13e8b9c2d97ac84829d07ba8511ec
Clive <viljoen.clive.g@gmail.com>
2021-03-09 16:24:37 +0200

    Updated database connect function

--- app/core/Database.php
+++ app/core/Database.php
@@ -23,33 +23,10 @@ class Database
       
     protected static function connect()
     {
-        $dsn = 'mysql:host=' . $dbHost . ';dbname=' . $dbName;
-        
-        $options = array(
-            //preventing driver timeouts when we are attempting to connect to the database, will also check if there is alreayd a connection with the database
-          PDO::ATTR_PERSISTENT => true,
-            //Way to handle errors
-          PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-        );
-        $pdo = new PDO($dsn, $dbUser, $dbPass, $options);
+        $dsn = 'mysql:host=' . $this->dbHost . ';dbname=' . $this->dbName;
+        $pdo = new PDO($dsn, $this->dbUser, $this->dbPass);
         $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
         return $pdo;
-        
-       //try 
-       //{
-            //creating and instance and setting pdo error mode to exception
-       // $dbhandler = new PDO($dsn, self::$dbUser, self::$dbPass, $options);
-            
-            //display success message
-       //  echo "You have successfully connected to the mysql server!";
-      // } 
-      // catch (PDOException $e) 
-       //{
-       
-       // $error = $e->getMessage();
-            //display error message
-        //echo "Connection failed, reason:".$error;
-      //}
     } 
 
  
